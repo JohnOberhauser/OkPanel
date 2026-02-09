81e54fb10489d170cbe1b6a205f275fe40fd68cd
John Oberhauser <j.git-global@obez.io>
2026-02-09 17:19:17 -0600

    updating notification ui

--- ags/widget/notification/Notification.tsx
+++ ags/widget/notification/Notification.tsx
@@ -40,7 +40,7 @@ export default function Notification(props: Props) {
             marginTop={2}
             orientation={Gtk.Orientation.HORIZONTAL}>
             <label
-                cssClasses={["labelSmallBold"]}
+                cssClasses={["labelSmallBold", "notificationTitle"]}
                 marginStart={8}
                 halign={START}
                 ellipsize={Pango.EllipsizeMode.END}
